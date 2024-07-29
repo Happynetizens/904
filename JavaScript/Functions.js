90088b663c15783f1a3b84ca73323ea2d34383ca
Happy netizens <120866998+Happynetizens@users.noreply.github.com>
2024-07-29 15:55:51 +0800

    2024.7.29
    添加照片-1
    控制隐藏的BUG改了一点

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -22,33 +22,6 @@ function IsShowing(ElementId) {
 	const rect = element.getBoundingClientRect();
 	return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth));
 }
-function findContent() {
-	for(let i=0; i<timings.length-1; i++) {
-		if(IsShowing(timings[i].place) && !IsShowing(timings[i+1].place)) {
-			return i;
-		}
-	}
-	return -1;
-}
-function CurbTab() {
-	const p = findContent();
-	const TabLast = document.getElementById('TabLast');
-	const TabThis = document.getElementById('TabThis');
-	const TabNext = document.getElementById('TabNext');
-	if(p === -1) {
-		TabLast.innerHTML = "";
-		TabThis.innerHTML = "";
-		TabNext.innerHTML = "";
-	} else {
-		if(p-1 >= 0) TabLast.innerHTML = timings[p-1].date;
-		else TabLast.innerHTML = "";
-		TabThis.innerHTML = timings[p].date;
-		if(p+1 < timings.length) TabNext.innerHTML = timings[p+1].date;
-		else TabNext.innerHTML = "";
-	}
-}
-setInterval(CurbTab, 0);
-
 function Inspect(p) {
 	for (let i = 0; i < useless.length; i++) {
 		if (useless[i] === timings[p].place) {
@@ -59,29 +32,76 @@ function Inspect(p) {
 }
 function theNext(place) {
 	place++;
-	if(Inspect(place)){
+	while (Inspect(place)) {
 		place++;
 	}
 	return place;
 }
 function theLast(place) {
 	place--;
-	if(Inspect(place)){
+	while (Inspect(place)) {
 		place--;
 	}
 	return place;
 }
-window.addEventListener('scroll', function() {
-	const currentScrollY = window.scrollY;
-	const scrollDelta = currentScrollY - lastScrollY;
-	if (scrollDelta > scrollThreshold) {
-		document.getElementById(timings[findContent()].place).className = "";
-		document.getElementById(timings[theNext(findContent())].place).className = "hidden";
-	} else if (scrollDelta < -scrollThreshold) {
-		document.getElementById(timings[findContent()].place).className = "";
-		document.getElementById(timings[theLast(findContent())].place).className = "hidden";
+function findContent() {
+	for (let i=0; i<timings.length-1; i++) {
+		if (!Inspect(i)) continue;
+		if (IsShowing(timings[i].place)) {
+			return i;
+		}
 	}
+	return -1;
+}
+function CurbTab() {
+	const p = findContent();
+	const TabLast = document.getElementById('TabLast');
+	const TabThis = document.getElementById('TabThis');
+	const TabNext = document.getElementById('TabNext');
+	if (p === -1) {
+		TabLast.innerHTML = "";
+		TabThis.innerHTML = "";
+		TabNext.innerHTML = "";
+	} else {
+		if (p-1 >= 0) TabLast.innerHTML = timings[p-1].date;
+		else TabLast.innerHTML = "";
+		TabThis.innerHTML = timings[p].date;
+		if (p+1 < timings.length) TabNext.innerHTML = timings[p+1].date;
+		else TabNext.innerHTML = "";
+	}
+}
+setInterval(CurbTab, 0);
+
+function CurbShow(way) {
+	let now = findContent();
+	if (way) {
+		console.log('+');
+		document.getElementById(timings[now].place).className = "hidden";
+		document.getElementById(timings[theNext(now)].place).className = "appear";
+	} else {
+		console.log('-');
+		document.getElementById(timings[now].place).className = "hidden";
+		document.getElementById(timings[theLast(now)].place).className = "appear";
+	}
+}
+window.addEventListener('scroll', function() {
+	const currentScrollY = window.scrollY || document.documentElement.scrollTop;
+	const scrollDelta = currentScrollY - lastScrollY;
+	if (scrollDelta > scrollThreshold) CurbShow(true);
+	if (scrollDelta < -scrollThreshold) CurbShow(false);
 	lastScrollY = currentScrollY;
+});
+window.addEventListener('keydown', function(event){
+	switch (event.key) {
+		case'ArrowDown': case'ArrowRight':
+		CurbShow(true);
+		break;
+		case'ArrowUp': case'ArrowLeft':
+		CurbShow(false);
+		break;
+		default:
+		break;
+	}
 });
 
 function PassingTime() {
@@ -93,19 +113,19 @@ function PassingTime() {
 	const minute = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
 	const second = Math.floor((time % (60 * 1000)) / 1000);
 	let PrintTime= "";
-	if(year > 0) PrintTime += year + "年";
-	if(month > 0) PrintTime += month + "个月";
-	if(day > 0) PrintTime += day + "天";
-	if(hour > 0) PrintTime += hour + "小时";
-	if(minute > 0) PrintTime += minute + "分钟";
-	if(second > 0) PrintTime += second + "秒";
+	if (year > 0) PrintTime += year + "年";
+	if (month > 0) PrintTime += month + "个月";
+	if (day > 0) PrintTime += day + "天";
+	if (hour > 0) PrintTime += hour + "小时";
+	if (minute > 0) PrintTime += minute + "分钟";
+	if (second > 0) PrintTime += second + "秒";
 	const Time = document.getElementById("Time");
-	if(Time!=null) Time.innerHTML = "我们的回忆就在" + PrintTime + "前";
+	if (Time!=null) Time.innerHTML = "我们的回忆就在" + PrintTime + "前";
 }
 
 function ShowTime() {
-	if(document.getElementById('Time')==null) {
-		if(Progress() >= 75 || isNaN(Progress())) {
+	if (document.getElementById('Time')==null) {
+		if (Progress() >= 75 || isNaN(Progress())) {
 			const P = document.createElement('p');
 			P.id = 'Time';
 			P.className = 'Time';
@@ -118,16 +138,12 @@ function ShowTime() {
 setInterval(ShowTime, 0);
 
 const Music = document.getElementById('Music');
-if (!flag) {
-	document.addEventListener('keydown', function () {
-		Music.play();
-		flag = true;
-	});
-	document.addEventListener('mousemove', function () {
-		Music.play();
-		flag = true;
-	});
-}
+document.addEventListener('keypress', function () {
+	Music.play();
+});
+document.addEventListener('mousemove', function () {
+	Music.play();
+});
 Music.addEventListener('ended', function () {
 	Music.src = Musics[++ThisMusic % Musics.length].url;
 	Music.play();
