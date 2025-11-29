fb46405e2b3b6c7f3574d4bddc24f78beec30a3b
线段line <120866998+Happynetizens@users.noreply.github.com>
2025-11-30 00:32:59 +0800

    1. 回归了时间 2. 微调了CSS next：压缩图片

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -30,8 +30,32 @@ window.addEventListener('keydown', function(event) {		// key control
 	}
 });
 
+const thePassTime = setInterval(() => {
+	const time = new Date() - new Date(2024, 6 - 1, 2, 21, 0, 0);
+	const year = Math.floor(time / (365 * 24 * 60 * 60 * 1000));
+	const month = Math.floor((time % (365 * 24 * 60 * 60 * 1000)) / (30 * 24 * 60 * 60 * 1000));
+	const day = Math.floor((time % (30 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000));
+	const hour = Math.floor((time % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+	const minute = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
+	const second = Math.floor((time % (60 * 1000)) / 1000);
+	let printTime= "";
+	if(year > 0) printTime += year + "年";
+	if(month > 0) printTime += month + "个月";
+	if(day > 0) printTime += day + "天";
+	if(hour > 0) printTime += hour + "小时";
+	if(minute > 0) printTime += minute + "分钟";
+	if(second > 0) printTime += second + "秒";
+	document.getElementById("time").innerHTML = "我们的回忆就在" + printTime + "前";
+	const timeBoxLeft = document.getElementById("time").getBoundingClientRect().left, titleBoxWidth = document.getElementById("title").getBoundingClientRect().width;
+	console.log("timeBoxLetf=" + (timeBoxLeft - 20) + "\n" + "titleBoxWidtht=" + titleBoxWidth);
+	if(titleBoxWidth > timeBoxLeft - 20) {
+		document.getElementById("time").remove();
+		clearInterval(thePassTime);
+	}
+}, 1000);
+
 document.getElementById('Music').addEventListener('ended', () => {
-	let music = document.getElementById('Music');
+	const music = document.getElementById('Music');
 	music.src = './Music/' + musicList[nowMusic++ % musicList.length];
 	music.play();
 });
