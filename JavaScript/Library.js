682db712b91725b4a651fbf37b8551390511d7a7
线段line <120866998+Happynetizens@users.noreply.github.com>
2025-11-29 19:58:28 +0800

    1.修改tab四个空格为制表符； 2. 删除了以前webstrong的.idea配置文件； 3. 对了，CSS还没更新，因为还没加内容； 4. **important**加入了测试数据

--- JavaScript/Library.js
+++ JavaScript/Library.js
@@ -1,5 +1,38 @@
 export const contentList = [
 	{tag: "", codes: `<p>Loading......</p>`},		// [0]: Error
+	{tag: "2023年9月1日", codes: `
+					<div id="BeginSchool">
+						<p><strong>看啊，那是23年刚刚开学时的我们</strong></p>
+						<p>那时候的记忆还深刻吗？</p>
+						<p>是否还记得自己的第一个同桌呢？</p>
+						<img src="./Images/初中开学.jpg" alt="" class="image1"/> <br/>
+					</div>
+	`},
+	{tag: "2023年9月18日", codes: `
+					<div id="army1">
+						<p><strong>这是第一次军训时的我们</strong></p>
+						<!-- more -->
+						<img src="./Images/7年级军训1.jpg" alt="" class="image2"/> <br/>
+						<img src="./Images/7年级军训2.jpg" alt="" class="image2"/> <br/>
+					</div>
+	`},
+	{tag: "2023年9月29日", codes: `
+					<div id="2023-SportsMeeting">
+						<!-- more -->
+						<p>还有为运动会付出的老师们</p>
+						<img src="./Images/7运动会.jpg" alt="" class="image2"/> <br/>
+						<!-- more -->
+					</div>
+	`},
+	{tag: "2023年11月", codes: `
+					<div id="7Exercise">
+						<p>有出类拔萃的运动会，也有集体配合的广播体操比赛</p>
+						<img src="./Images/广播体操2.jpg" alt="" class="image3"/>
+						<img src="./Images/广播体操1.jpg" alt="" class="image3"/>
+						<p>这是我们集体的荣誉！</p>
+						<img src="./Images/广播体操3.jpg" alt="" class="image3"/>
+					</div>
+	`},
 	{tag: "1", codes: ``},
 	{tag: "2", codes: ``},
 	{tag: "3", codes: ``},
